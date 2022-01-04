f1a299bef7efcd9796a619d33553e4367904e1a8
uweweng <uwe.weng@gmx.de>
2022-01-04 11:58:55 +0100

    JavaBeans implementation is not supported

--- src/beans/src/main/java/net/sourceforge/fuzzyservices/beans/package-info.java
+++
@@ -1,6 +0,0 @@
-@XmlAccessorType(XmlAccessType.FIELD)    
-package net.sourceforge.fuzzyservices.beans;
-
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-
